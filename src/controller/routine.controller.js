f830e619b6644c27f743de2f55ded762ad681e78
Sabtha Rishi <sabtharishi01@gmail.com>
2022-08-12 14:04:10 +0530

    schema and crud for routines

---
+++ src/controller/routine.controller.js
@@ -0,0 +1,151 @@
+const RoutineModel = require("../model/routine.model");
+const TodoModel = require("../model/todo.model");
+const jwt = require("jsonwebtoken");
+
+const JWT_SECRET =
+  "sKSDwsbdkJH&@#&297298ydkjhsdfqw83yr2893y(*YWuerh238ry0(U&)(09q3r209fwkjhfehJH}{}WJe38rywehfj))";
+
+// Create List
+
+const create = (req, res) => {
+  const userID = jwt.verify(req.cookies.token, JWT_SECRET).id;
+
+  let newRoutine = new RoutineModel(req.body);
+  newRoutine.userID = userID;
+
+  newRoutine.save((err, routine) => {
+    if (err) {
+      return res.json({
+        success: false,
+        err: err.message,
+      });
+    }
+    return res.json({
+      success: true,
+      routine: routine,
+    });
+  });
+};
+
+// Add todo to list
+const addToRoutine = (req, res) => {
+  const userID = jwt.verify(req.cookies.token, JWT_SECRET).id;
+
+  let newTodo = new TodoModel();
+  newTodo.task = req.body.task;
+  newTodo.userID = userID;
+  newTodo.isHidden = true;
+
+  newTodo.save((err, todo) => {
+    if (err) {
+      return res.json({
+        success: false,
+        err: err.message,
+      });
+    }
+
+    // Todo Created
+
+    RoutineModel.findByIdAndUpdate(
+      req.body.routineID,
+      { $push: { tasks: todo._id } },
+      { new: true },
+      (err, routine) => {
+        if (err) {
+          return res.json({
+            success: false,
+            err: err.message,
+          });
+        }
+
+        // Todo added to the list
+        return res.json({
+          success: true,
+          routine: routine,
+        });
+      }
+    );
+  });
+};
+
+const allRoutines = (req, res) => {
+  const userID = jwt.verify(req.cookies.token, JWT_SECRET).id;
+  RoutineModel.find({ userID: userID }, (err, routines) => {
+    if (err) {
+      return res.json({
+        success: false,
+        err: err.message,
+      });
+    }
+    return res.json({
+      success: true,
+      routines: routines,
+    });
+  });
+};
+
+const getRoutine = (req, res) => {
+  const userID = jwt.verify(req.cookies.token, JWT_SECRET).id;
+
+  RoutineModel.findById(req.params.routineID, (err, routine) => {
+    if (err) {
+      return res.json({
+        success: false,
+        err: err.message,
+      });
+    }
+    if (list.userID != userID) {
+      return res.json({
+        success: false,
+        err: "Not authenticated",
+      });
+    }
+
+    TodoModel.find()
+      .where("_id")
+      .in(routine.tasks)
+      .exec((err, tasksData) => {
+        if (err) {
+          return res.json({
+            success: false,
+            err: err.message,
+          });
+        }
+        return res.status(201).json({
+          success: true,
+          routine: routine,
+          tasks: tasksData,
+        });
+      });
+  });
+};
+
+const update = (req, res) => {
+  const routineID = req.params.routineID;
+  RoutineModel.findByIdAndUpdate(
+    routineID,
+    req.body,
+    { new: true },
+    (err, routine) => {
+      if (err) {
+        return res.json({
+          success: false,
+          err: err.message,
+        });
+      }
+      return res.json({
+        success: true,
+        routine: routine,
+      });
+    }
+  );
+};
+
+const RoutineController = {
+  create,
+  addToRoutine,
+  allRoutines,
+  getRoutine,
+  update,
+};
+module.exports = RoutineController;
---
+++ src/model/routine.model.js
@@ -0,0 +1,53 @@
+// IMPORTS
+const mongoose = require("mongoose");
+
+// MODEL SCHEMA
+const RoutineSchema = new mongoose.Schema({
+  userID: {
+    type: String,
+    required: true,
+  },
+  name: {
+    required: true,
+    type: String,
+  },
+  started_at: {
+    type: Date,
+    require: true,
+    default: Date.now(),
+  },
+  tasks: [
+    {
+      type: String,
+    },
+  ],
+  isArchieved: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  isCompleted: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  isStarted: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  isEnded: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+});
+// ______________________________________ SCHEMA END  ____________________________
+//  _____________________________________ MODEL METHODS START _____________________
+
+RoutineSchema.methods = {};
+
+//  _____________________________________ MODEL METHODS END ______________________
+//  _________________________________________ EXPORTS _____________________________
+
+const Routine = (module.exports = mongoose.model("Routine", RoutineSchema));
